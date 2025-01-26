2d31b766c31d2ec6ef2792095a1b3ea0d448b595
hzy_2012 <3681596340@qq.com>
2025-01-26 11:13:19 +0800

    Add files via upload

---
+++ 数据库 1.0.0.cpp
@@ -0,0 +1,689 @@
+void k(int z,int out) {
+	switch(z) {
+		case 1: {
+			x=1;
+			s="橡木原木";
+			break;
+		}
+		case 2: {
+			x=2;
+			s="橡木";
+			break;
+		}
+		case 3: {
+			x=3;
+			s="去皮橡木原木";
+			break;
+		}
+		case 4: {
+			x=1;
+			s="去皮橡木";
+			break;
+		}
+		case 5: {
+			x=5;
+			s="橡木木板";
+			break;
+		}
+		case 6: {
+			x=6;
+			s="橡木楼梯";
+			break;
+		}
+		case 7: {
+			x=7;
+			s="橡木台阶";
+			break;
+		}
+		case 8: {
+			x=8;
+			s="橡木栅栏";
+			break;
+		}
+		case 9: {
+			x=9;
+			s="橡木栅栏门";
+			break;
+		}
+		case 10: {
+			x=10;
+			s="橡木门";
+			break;
+		}
+		case 11: {
+			x=11;
+			s="橡木活门板";
+			break;
+		}
+		case 12: {
+			x=12;
+			s="橡木压力板";
+			break;
+		}
+		case 13: {
+			x=13;
+			s="橡木按钮";
+			break;
+		}
+		case 14: {
+			x=1;
+			s="云杉原木";
+			break;
+		}
+		case 15: {
+			x=15;
+			s="云杉木";
+			break;
+		}
+		case 16: {
+			x=16;
+			s="去皮云杉原木";
+			break;
+		}
+		case 17: {
+			x=17;
+			s="去皮云杉";
+			break;
+		}
+		case 18: {
+			x=18;
+			s="云杉木板";
+			break;
+		}
+		case 19: {
+			x=6;
+			s="云杉楼梯";
+			break;
+		}
+		case 20: {
+			x=20;
+			s="云杉台阶";
+			break;
+		}
+		case 21: {
+			x=21;
+			s="云杉栅栏";
+			break;
+		}
+		case 22: {
+			x=22;
+			s="云杉栅栏门";
+			break;
+		}
+		case 23: {
+			x=23;
+			s="云杉门";
+			break;
+		}
+		case 24: {
+			x=24;
+			s="云杉活门板";
+			break;
+		}
+		case 25: {
+			x=25;
+			s="云杉压力板";
+			break;
+		}
+		case 26: {
+			x=26;
+			s="云杉按钮";
+			break;
+		}
+		case 27: {
+			x=27;
+			s="白桦原木";
+			break;
+		}
+		case 28: {
+			x=28;
+			s="白桦木";
+			break;
+		}
+		case 29: {
+			x=29;
+			s="去皮白桦原木";
+			break;
+		}
+		case 30: {
+			x=30;
+			s="去皮白桦";
+			break;
+		}
+		case 31: {
+			x=31;
+			s="白桦木板";
+			break;
+		}
+		case 32: {
+			x=32;
+			s="白桦楼梯";
+			break;
+		}
+		case 33: {
+			x=33;
+			s="白桦台阶";
+			break;
+		}
+		case 34: {
+			x=34;
+			s="白桦栅栏";
+			break;
+		}
+		case 35: {
+			x=35;
+			s="白桦栅栏门";
+			break;
+		}
+		case 36: {
+			x=36;
+			s="白桦门";
+			break;
+		}
+		case 37: {
+			x=37;
+			s="白桦活门板";
+			break;
+		}
+		case 38: {
+			x=38;
+			s="白桦压力板";
+			break;
+		}
+		case 39: {
+			x=39;
+			s="白桦按钮";
+			break;
+		}
+		case 40: {
+			x=40;
+			s="丛林原木";
+			break;
+		}
+		case 41: {
+			x=41;
+			s="丛林木";
+			break;
+		}
+		case 42: {
+			x=42;
+			s="去皮丛林原木";
+			break;
+		}
+		case 43: {
+			x=43;
+			s="去皮丛林";
+			break;
+		}
+		case 44: {
+			x=44;
+			s="丛林木板";
+			break;
+		}
+		case 45: {
+			x=45;
+			s="丛林楼梯";
+			break;
+		}
+		case 46: {
+			x=46;
+			s="丛林台阶";
+			break;
+		}
+		case 47: {
+			x=47;
+			s="丛林栅栏";
+			break;
+		}
+		case 48: {
+			x=48;
+			s="丛林栅栏门";
+			break;
+		}
+		case 49: {
+			x=49;
+			s="丛林门";
+			break;
+		}
+		case 50: {
+			x=50;
+			s="丛林活门板";
+			break;
+		}
+		case 51: {
+			x=51;
+			s="丛林压力板";
+			break;
+		}
+		case 52: {
+			x=52;
+			s="丛林按钮";
+			break;
+		}
+		case 53: {
+			x=53;
+			s="金合欢原木";
+			break;
+		}
+		case 54: {
+			x=54;
+			s="金合欢木";
+			break;
+		}
+		case 55: {
+			x=55;
+			s="去皮金合欢原木";
+			break;
+		}
+		case 56: {
+			x=56;
+			s="去皮金合欢";
+			break;
+		}
+		case 57: {
+			x=57;
+			s="金合欢木板";
+			break;
+		}
+		case 58: {
+			x=58;
+			s="金合欢楼梯";
+			break;
+		}
+		case 59: {
+			x=59;
+			s="金合欢台阶";
+			break;
+		}
+		case 60: {
+			x=60;
+			s="金合欢栅栏";
+			break;
+		}
+		case 61: {
+			x=61;
+			s="金合欢栅栏门";
+			break;
+		}
+		case 62: {
+			x=62;
+			s="金合欢门";
+			break;
+		}
+		case 63: {
+			x=63;
+			s="金合欢活门板";
+			break;
+		}
+		case 64: {
+			x=64;
+			s="金合欢压力板";
+			break;
+		}
+		case 65: {
+			x=65;
+			s="金合欢按钮";
+			break;
+		}
+		case 66: {
+			x=66;
+			s="深色橡木原木";
+			break;
+		}
+		case 67: {
+			x=67;
+			s="深色橡木";
+			break;
+		}
+		case 68: {
+			x=68;
+			s="去皮深色橡木原木";
+			break;
+		}
+		case 69: {
+			x=69;
+			s="去皮深色橡木";
+			break;
+		}
+		case 70: {
+			x=70;
+			s="深色橡木木板";
+			break;
+		}
+		case 71: {
+			x=71;
+			s="深色橡木楼梯";
+			break;
+		}
+		case 72: {
+			x=72;
+			s="深色橡木台阶";
+			break;
+		}
+		case 73: {
+			x=73;
+			s="深色橡木栅栏";
+			break;
+		}
+		case 74: {
+			x=74;
+			s="深色橡木栅栏门";
+			break;
+		}
+		case 75: {
+			x=75;
+			s="深色橡木门";
+			break;
+		}
+		case 76: {
+			x=76;
+			s="深色橡木活门板";
+			break;
+		}
+		case 77: {
+			x=77;
+			s="深色橡木压力板";
+			break;
+		}
+		case 78: {
+			x=78;
+			s="深色橡木按钮";
+			break;
+		}
+		case 79: {
+			x=79;
+			s="红树原木";
+			break;
+		}
+		case 80: {
+			x=80;
+			s="红树木";
+			break;
+		}
+		case 81: {
+			x=81;
+			s="去皮红树原木";
+			break;
+		}
+		case 82: {
+			x=82;
+			s="去皮红树";
+			break;
+		}
+		case 83: {
+			x=83;
+			s="红树木板";
+			break;
+		}
+		case 84: {
+			x=84;
+			s="红树楼梯";
+			break;
+		}
+		case 85: {
+			x=85;
+			s="红树台阶";
+			break;
+		}
+		case 86: {
+			x=86;
+			s="红树栅栏";
+			break;
+		}
+		case 87: {
+			x=87;
+			s="红树栅栏门";
+			break;
+		}
+		case 88: {
+			x=88;
+			s="红树门";
+			break;
+		}
+		case 76: {
+			x=76;
+			s="红树活门板";
+			break;
+		}
+		case 77: {
+			x=77;
+			s="红树压力板";
+			break;
+		}
+		case 78: {
+			x=78;
+			s="红树按钮";
+			break;
+		}
+		case 92: {
+			x=92;
+			s="樱花原木";
+			break;
+		}
+		case 93: {
+			x=93;
+			s="樱花木";
+			break;
+		}
+		case 94: {
+			x=94;
+			s="去皮樱花原木";
+			break;
+		}
+		case 95: {
+			x=95;
+			s="去皮樱花";
+			break;
+		}
+		case 96: {
+			x=96;
+			s="樱花木板";
+			break;
+		}
+		case 97: {
+			x=97;
+			s="樱花楼梯";
+			break;
+		}
+		case 98: {
+			x=98;
+			s="樱花台阶";
+			break;
+		}
+		case 99: {
+			x=99;
+			s="樱花栅栏";
+			break;
+		}
+		case 100: {
+			x=100;
+			s="樱花栅栏门";
+			break;
+		}
+		case 101: {
+			x=101;
+			s="樱花门";
+			break;
+		}
+		case 102: {
+			x=102;
+			s="樱花活门板";
+			break;
+		}
+		case 103: {
+			x=103;
+			s="樱花压力板";
+			break;
+		}
+		case 104: {
+			x=104;
+			s="樱花按钮";
+			break;
+		}
+		case 105: {
+			x=105;
+			s="苍白橡木原木";
+			break;
+		}
+		case 106: {
+			x=106;
+			s="苍白橡木";
+			break;
+		}
+		case 107: {
+			x=107;
+			s="去皮苍白橡木原木";
+			break;
+		}
+		case 108: {
+			x=108;
+			s="去皮苍白橡木";
+			break;
+		}
+		case 109: {
+			x=109;
+			s="苍白橡木木板";
+			break;
+		}
+		case 110: {
+			x=110;
+			s="苍白橡木楼梯";
+			break;
+		}
+		case 111: {
+			x=111;
+			s="苍白橡木台阶";
+			break;
+		}
+		case 112: {
+			x=112;
+			s="苍白橡木栅栏";
+			break;
+		}
+		case 113: {
+			x=113;
+			s="苍白橡木栅栏门";
+			break;
+		}
+		case 114: {
+			x=114;
+			s="苍白橡木门";
+			break;
+		}
+		case 115: {
+			x=115;
+			s="苍白橡木活门板";
+			break;
+		}
+		case 116: {
+			x=116;
+			s="苍白橡木压力板";
+			break;
+		}
+		case 117: {
+			x=117;
+			s="苍白橡木按钮";
+			break;
+		}
+		case 118: {
+			x=118;
+			s="诡异菌柄";
+			break;
+		}
+		case 119: {
+			x=119;
+			s="诡异菌核";
+			break;
+		}
+		case 120: {
+			x=120;
+			s="去皮诡异菌原木";
+			break;
+		}
+		case 121: {
+			x=121;
+			s="去皮诡异菌";
+			break;
+		}
+		case 122: {
+			x=122;
+			s="诡异菌木板";
+			break;
+		}
+		case 123: {
+			x=123;
+			s="诡异菌楼梯";
+			break;
+		}
+		case 124: {
+			x=124;
+			s="诡异菌台阶";
+			break;
+		}
+		case 125: {
+			x=125;
+			s="诡异菌栅栏";
+			break;
+		}
+		case 126: {
+			x=126;
+			s="诡异菌栅栏门";
+			break;
+		}
+		case 127: {
+			x=127;
+			s="诡异菌门";
+			break;
+		}
+		case 128: {
+			x=128;
+			s="诡异菌活门板";
+			break;
+		}
+		case 129: {
+			x=129;
+			s="诡异菌压力板";
+			break;
+		}
+		case 130: {
+			x=130;
+			s="诡异菌按钮";
+			break;
+		}
+		case 131: {
+			x=131;
+			s="原石";
+			break;
+		}
+		case 132: {
+			x=132;
+			s="煤矿";
+			break;
+		}
+		case 133: {
+			x=133;
+			s="铁矿";
+			break;
+		}
+		case 134: {
+			x=134;
+			s="金矿";
+			break;
+		}
+		case 135: {
+			x=135;
+			s="钻石矿";
+			break;
+		}
+		case 136: {
+			x=136;
+			s="基岩";
+			break;
+		}
+		case 137: {
+			x=137;
+			s="沙子";
+			break;
+		}
+	}
+}
